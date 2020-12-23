10dcf9e38002a22049b077f70754dd0633a6bdf6
Dustin Simensen <dustinsimensen@gmail.com>
2020-12-23 15:29:47 -0800

    fuck you

--- frontend/gatsby-browser.js
+++ frontend/gatsby-browser.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import IndexLayout from './src/layouts/Index';
+import IndexLayout from '../frontend/src/layouts/IndexLayout';
 
 export const wrapPageElement = ({ element, props }) => (
   <IndexLayout {...props}>{element}</IndexLayout>
