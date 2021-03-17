79fee9dae6a83314343796e4892574ac67d8afb2
Dustin Simensen <dustinsimensen@gmail.com>
2021-03-17 10:42:14 -0700

    more footer styles

--- frontend/src/components/FooterSocialMedia.tsx
+++ frontend/src/components/FooterSocialMedia.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useStyletron } from 'baseui';
 import { Block } from 'baseui/block';
-import { H5 } from 'baseui/typography';
+import { H3, H5 } from 'baseui/typography';
 import {
   FiMail as Mail,
   FiYoutube as Youtube,
@@ -12,6 +12,35 @@ import {
 
 const FooterSocialMedia = () => {
   const [css] = useStyletron();
+  const children = (
+    <>
+      <Twitter
+        className={css({
+          marginRight: '1rem',
+        })}
+      />
+      <Facebook
+        className={css({
+          marginRight: '1rem',
+        })}
+      />
+      <Instagram
+        className={css({
+          marginRight: '1rem',
+        })}
+      />
+      <Youtube
+        className={css({
+          marginRight: '1rem',
+        })}
+      />
+      <Mail
+        className={css({
+          marginRight: '1rem',
+        })}
+      />
+    </>
+  );
   return (
     <Block
       paddingLeft={['1rem', '1rem', '0']}
@@ -19,33 +48,10 @@ const FooterSocialMedia = () => {
       display="flex"
       justifyContent={['center', 'center', 'flex-end']}
     >
-      <H5>
-        <Twitter
-          className={css({
-            marginRight: '1rem',
-          })}
-        />
-        <Facebook
-          className={css({
-            marginRight: '1rem',
-          })}
-        />
-        <Instagram
-          className={css({
-            marginRight: '1rem',
-          })}
-        />
-        <Youtube
-          className={css({
-            marginRight: '1rem',
-          })}
-        />
-        <Mail
-          className={css({
-            marginRight: '1rem',
-          })}
-        />
-      </H5>
+      <H5 display={['none', 'none', 'block', 'block']}>{children}</H5>
+      <H3 display={['block', 'block', 'none', 'none']} margin={0}>
+        {children}
+      </H3>
     </Block>
   );
 };
